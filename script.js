b57900416266d35f24b5dc71f7c9e3f171dc1b48
giulia-buonasperanza <giulia.9402@gmail.com>
2026-03-31 16:08:00 +0200

    funzione per verificare se una parola è palindroma

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
+
+//FUNZIONE
+
+function esserePalindomo (parola) {
+    let isPalindromo = true;
+    for (i=0; i < parola.length / 2 && isPalindromo; i++){
+    const inizio = parola[i];
+    const fine = parola[parola.length - 1 - i];  
+    
+     if (inizio !== fine) {
+            isPalindromo = false;
+        }
+    }
+
+     if (isPalindromo) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//MAIN
